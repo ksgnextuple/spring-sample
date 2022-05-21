39b4d6d88a8f06d4b251e2fb0fb21f938213cbdf
GouthamSekar <goutham.sekar@nextuple.com>
2022-05-21 23:46:45 +0530

    Isolating Build and Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo ${{ ECR_REGISTRY }}
+echo $ECR_REGISTRY
 
+cd initial
+export SERVICE_NAME=`echo $GITHUB_REPOSITORY | awk -F "/" '{print $2}'`
+export VERSION=`bash ./gradlew -Pbuild_target=SNAPSHOT -q properties | grep version | sed -e "s@version: @@g"`
+export SERVICE_CODE=`echo "$VERSION" | cut -d '-' -f1`
+export IMAGE_NAME="$ECR_REGISTRY/$SERVICE_NAME:$SERVICE_CODE.$COMMIT_HASH"
+cd ..
+
+echo "$IMAGE_NAME"
 
 aws sts get-caller-identity
 aws eks update-kubeconfig --name eksdemo1 --region us-east-1
