3e8dac8f8a37c11886862e22e42dbc4e0674f002
GouthamSekar <goutham.sekar@nextuple.com>
2022-05-22 12:49:08 +0530

    Added Config file and yq tool

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,12 @@ cd ..
 echo "$IMAGE_NAME"
 
 aws sts get-caller-identity
-aws eks update-kubeconfig --name eksdemo1 --region us-east-1
+#aws eks update-kubeconfig --name eksdemo1 --region us-east-1
 
-export ENVIRONMENT="dev"
 export COLOR1="blue"
 export COLOR2="green"
 
-export SERVICE_ACTIVE_COLOR=$(kubectl get svc rest-uservice -n $ENVIRONMENT -o jsonpath='{.spec.selector.color}' --ignore-not-found=true)
+#export SERVICE_ACTIVE_COLOR=$(kubectl get svc rest-uservice -n $ENVIRONMENT -o jsonpath='{.spec.selector.color}' --ignore-not-found=true)
 
 if [ "$SERVICE_ACTIVE_COLOR" == "$COLOR1" ]; then
   export SERVICE_PASSIVE_COLOR="$COLOR2"
@@ -28,6 +27,7 @@ fi
 
 echo "Currently running deployment color : $SERVICE_ACTIVE_COLOR"
 
+yq --version
 
 #kubectl apply -f ./manifests/deployment.yaml
 #kubectl apply -f ./manifests/service.yaml
