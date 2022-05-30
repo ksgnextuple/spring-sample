fd7f11899fe4914627b8e7841231b7d8d182cc7a
GouthamSekar <goutham.sekar@nextuple.com>
2022-05-30 23:53:20 +0530

    Added reusable pr flow

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Comments
 @SpringBootApplication
 public class RestServiceApplication {
 
