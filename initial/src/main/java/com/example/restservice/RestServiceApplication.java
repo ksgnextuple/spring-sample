6457b68b5c3b485e1f4a5c964226d7c61dfe9716
GouthamSekar <goutham.sekar@nextuple.com>
2022-06-03 10:30:49 +0530

    Updated pipeline with the permissions

--- initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//comment
 @SpringBootApplication
 public class RestServiceApplication {
 
